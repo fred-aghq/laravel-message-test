3f44116fd9dcfee95d84fcbfd762bf768cc6fb84
Fred Spooner <fred@aghq.uk>
2021-04-21 16:59:08 +0100

    feat: add a little style; add mark-read, archive buttons

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,27 +1,29 @@
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="/css/app.css" rel="stylesheet">
-</head>
-<body>
-<h1 class="text-green-200">Tailwind worked</h1>
-</body>
+@include('header')
 <div class="container mx-auto md:max-w-2xl">
-    <div id="nav-links">
-        <ul>
-            <li>Create a new message</li>
-            <li>View all messages (archived/read)</li>
-            <li></li>
-        </ul>
-    </div>
     @foreach ($messages as $message)
-        <div class="p-10">
+        <div class="p-10 {{$message['read'] ? '' : 'text-red-500'}}">
             <h1 class="text-2xl">{{$message['subject']}}</h1>
-            <h2 class=" text-sm text-gray-400">Sent: {{$message['sent']}}</h2>
+            <h2 class="text-sm text-gray-400">Sent: {{$message['sent']}}</h2>
             @if($message['archived'])
-                <h2>Archived: {{$message['archived']}}</h2>
+                <h2 class="text-sm text-gray-400">Archived: {{$message['archived']}}</h2>
             @endif
-            <p>{{$message['body']}}</p>
+            @if($message['read'])
+                <h2 class="text-sm text-gray-400"></h2>
+            @endif
+            <form action="/messages/{{$message['id']}}" method="POST">
+                @csrf
+                @method('PATCH')
+                <input name="archive" type="hidden" value={{ (int) !$message['archived'] }} />
+                <button type="submit">{{ $message['archived'] ? 'Unarchive' : 'Archive' }}</button>
+            </form>
+            <form action="/messages/{{$message['id']}}" method="POST">
+                @csrf
+                @method('PATCH')
+                <input name="read" type="hidden" value={{ (int) !$message['read'] }} />
+                <button type="submit">{{ $message['read'] ? 'Mark as unread' : 'Mark as read' }}</button>
+            </form>
+            <p class="text-md">{{$message['body']}}</p>
         </div>
     @endforeach
 </div>
+@include('footer')
