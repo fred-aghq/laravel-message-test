bd375e516b718c50c333017903ec3cfecd4ab672
Fred Spooner <fred@aghq.uk>
2021-04-21 14:43:50 +0100

    chore: initial commit. feat: sets root to display inbox view. feat: message migration file

---
+++ database/migrations/2021_04_21_134002_create_messages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('subject')
+                ->nullable();
+            $table->text('body');
+            $table->dateTime('sent')
+                ->nullable();
+            $table->dateTime('read')
+                ->nullable();
+            $table->dateTime('archived')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+
+}
