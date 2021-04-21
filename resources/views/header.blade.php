9a95e0b4a651119fec4608544c7691403d29a1ca
Fred Spooner <fred@aghq.uk>
2021-04-21 17:35:24 +0100

    feat: improve filtering and navigation

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,12 +5,21 @@
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
-<div id="nav-links">
-    <ul class="text-2xl">
-        <li class="text-green-400">
-            <a href="/messages/create">Create a new message</li>
-        <li class="text-yellow-400">
-            <a href="/messages?all=1">View all messages (archived/read)</a></li>
-        <li></li>
-    </ul>
+<div class="container mx-auto">
+    <div id="nav-links">
+        <ul class="text-2xl p-10">
+            <li>
+                <a href="/messages">Inbox</a>
+            </li>
+            <li class="text-green-400">
+                <a href="/messages/create">Create a new message</a>
+            </li>
+            <li class="text-red-500">
+                <a href="/messages?unread=1">Unread messages</a>
+            </li>
+            <li class="text-yellow-400">
+                <a href="/messages?all=1">View all messages (archived/read)</a>
+            </li>
+        </ul>
+    </div>
 </div>
