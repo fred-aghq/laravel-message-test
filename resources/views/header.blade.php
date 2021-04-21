8d09da755d242c4a305418ccb01851e88e2cf56c
Fred Spooner <fred@aghq.uk>
2021-04-21 16:56:50 +0100

    feat: create header and footer blades to improve code reuse

---
+++ resources/views/header.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+<div id="nav-links">
+    <ul class="text-2xl">
+        <li class="text-green-400">
+            <a href="/messages/create">Create a new message</li>
+        <li class="text-yellow-400">
+            <a href="/messages?all=1">View all messages (archived/read)</a></li>
+        <li></li>
+    </ul>
+</div>
