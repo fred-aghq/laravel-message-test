aa16a4a9d58070bd1be970aac05e30fe0f3f82eb
Fred Spooner <fred@aghq.uk>
2021-04-21 17:35:10 +0100

    feat: improve UI style a little on index template

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,8 +1,8 @@
 @include('header')
 <div class="container mx-auto md:max-w-2xl">
     @foreach ($messages as $message)
-        <div class="p-10 {{$message['read'] ? '' : 'text-red-500'}}">
-            <h1 class="text-2xl">{{$message['subject']}}</h1>
+        <div class="p-6 mb-4 border-2 border-gray-200">
+            <h1 class="{{$message['read'] ? '' : 'text-red-500'}} text-2xl">{{$message['subject'] ?? '< no subject >'}}</h1>
             <h2 class="text-sm text-gray-400">Sent: {{$message['sent']}}</h2>
             @if($message['archived'])
                 <h2 class="text-sm text-gray-400">Archived: {{$message['archived']}}</h2>
@@ -13,14 +13,14 @@
             <form action="/messages/{{$message['id']}}" method="POST">
                 @csrf
                 @method('PATCH')
-                <input name="archive" type="hidden" value={{ (int) !$message['archived'] }} />
-                <button type="submit">{{ $message['archived'] ? 'Unarchive' : 'Archive' }}</button>
+                <input name="archive" type="hidden" value="{{ (int) !$message['archived'] }}" />
+                <button class="py-1 text-sm px-4 border-blue border-2 bg-blue-200" type="submit">{{ $message['archived'] ? 'Unarchive' : 'Archive' }}</button>
             </form>
             <form action="/messages/{{$message['id']}}" method="POST">
                 @csrf
                 @method('PATCH')
                 <input name="read" type="hidden" value={{ (int) !$message['read'] }} />
-                <button type="submit">{{ $message['read'] ? 'Mark as unread' : 'Mark as read' }}</button>
+                <button class="y-1 text-sm px-4 border-blue border-2 bg-blue-200" type="submit">{{ $message['read'] ? 'Mark as unread' : 'Mark as read' }}</button>
             </form>
             <p class="text-md">{{$message['body']}}</p>
         </div>
