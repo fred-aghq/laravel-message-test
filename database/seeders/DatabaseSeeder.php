a14c48ff323d22b4b4698584d94ce09344928001
Fred Spooner <fred@aghq.uk>
2021-04-21 15:31:01 +0100

    feat: configure DatabaseSeeder to give us some messages to play with

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\Message::factory(25)->create();
+         \App\Models\Message::factory(25)->archived()->create();
+         \App\Models\Message::factory(25)->read()->create();
     }
 }
