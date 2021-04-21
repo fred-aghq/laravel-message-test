fd0c664c1c1d01bb0061f2c648d7db591b8572dd
Fred Spooner <fred@aghq.uk>
2021-04-21 15:31:17 +0100

    feat: add messages entrypoint for MessageController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('inbox');
-});
-
+Route::resource('/messages', MessageController::class);
