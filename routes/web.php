9ae2bb25ce51f8243a8dda20b8a4df536fedc7a5
Fred Spooner <fred@aghq.uk>
2021-04-21 17:34:32 +0100

    chore: disable unused routes

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::redirect('/', '/messages');
-Route::resource('/messages', MessageController::class);
+Route::resource('/messages', MessageController::class)
+    ->except(['show', 'destroy', 'edit']);
