3c485d9114878250fb744dff080f8214c1f9cbe7
Fred Spooner <fred@aghq.uk>
2021-04-21 15:28:52 +0100

    feat: add MessageController with some basic store + validate, index, and show methods

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $messages = Message::query();
+
+        if ((int) $request->get('all') !== 1) {
+            $messages->whereNull('read');
+            $messages->whereNull('archived');
+        }
+
+        return view('messages.index', ['messages' => $messages->get()->toArray()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('messages.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->validate([
+            'subject' => 'sometimes|max:20',
+            'body' => 'required|max:240',
+        ]);
+
+        $message = new Message($input);
+
+        $message->sent = Carbon::now();
+        $message->save();
+        return response($message, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('messages.show', ['message' => Message::findOrFail($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
